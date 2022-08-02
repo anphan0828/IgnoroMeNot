1807f2f9c5c6aa9d4acf82153b2fd165954690c5
anphan0828 <ahphan@iastate.edu>
2022-08-02 11:25:23 -0500

    Update docstring

--- ignoromenot.py
+++ ignoromenot.py
@@ -31,7 +31,9 @@ request_url = "/".join([STRING_API_URL, output_format, method])
 
 
 def parse_commandlinearguments():
-    """Parsing command line arguments: required input files and mutually exclusive thresholds."""
+    """Parsing command line arguments: required input files: rank tsv, STRING alias txt and STRING protein.links txt,
+    and input threshold (otherwise use default).
+    """
     parser = argparse.ArgumentParser(prog="ignoromenot.py")
     mutex_parser_top_thresh = parser.add_mutually_exclusive_group()
     mutex_parser_bot_thresh = parser.add_mutually_exclusive_group()
@@ -73,27 +75,34 @@ def parse_commandlinearguments():
 #         print()
 
 
-def rank_gaf_file():
-    # currently, take WyattClarkIC-perprotein.tsv as input (UniProt protein names and WC_IC)
+def rank_gaf_file(gaf_input):
     # TODO: handle multiple input files (enumerate(options.input))
     """Takes 1 GAF file as input and return ranked data frame of 4 per-protein metrics:
     annotation count, article count, information content, propagated information content
     """
-    gafoutput = GOA._gaf20iterator(open(eachinputfile, "r"))
-    data = convertFromGAFToRequiredFormat(gafoutput)
-    return
+    gaf_output = GOA._gaf20iterator(open(gaf_input, "r"))  # generator object, each row is a dict
+    # TODO: aggregate metrics per protein and get ranking
+    return gaf_output
 
 
 def filter_most_annotated(ranktable, ttop, ptop):
+    """
+    This function filters most annotated genes based on threshold
+    :param ranktable: tab-separated gene list with one or more metrics
+    :param ttop: absolute threshold
+    :param ptop: percentile threshold
+    :return: list of top genes
+    """
     allrank = pd.read_csv(ranktable, sep="\t", header=0)
-    # TODO: resolve NaN (add UniProt ID table)
     # TODO: create dict metric["prot"] = ["string_id", "preferred_name", "count", "wc_ic", ...]
     most_annt_list = []
+
+    # Get threshold and filter for each metric
     if ptop is not None:
-        threshold = np.percentile(allrank.iloc[:,1], float(ptop))
+        threshold = np.percentile(allrank.iloc[:, 1], float(ptop))
     else:
         threshold = float(ttop)
-    for index, row in allrank.iterrows():
+    for index, row in allrank.iterrows():  # another loop if >1 metric
         if row[1] >= threshold:
             most_annt_list.append(row[0])
     print("IC threshold for most annotated genes:", threshold)
@@ -101,16 +110,22 @@ def filter_most_annotated(ranktable, ttop, ptop):
 
 
 def filter_least_annotated(ranktable, tbot, pbot):
-    allrank = pd.read_csv(ranktable, sep="\t", header=0)  # handle many metrics
-    # ranked_missing = rankIC_stringID[rankIC_stringID.isna().any(axis=1)]
-    # TODO: resolve NaN (add UniProt ID table)
-    # TODO: create dict metric["prot"] = ["string_id", "preferred_name", "count", "wc_ic", ...]
+    """
+    This function filters least annotated genes based on threshold
+    :param ranktable: tab-separated gene list with one or more metrics
+    :param tbot: absolute threshold
+    :param pbot: percentile threshold
+    :return: list of bot genes
+    """
+    allrank = pd.read_csv(ranktable, sep="\t", header=0)
     least_annt_list = []
+
+    # Get threshold and filter for each metric
     if pbot is not None:
-        threshold = np.percentile(allrank.iloc[:,1], float(pbot))
+        threshold = np.percentile(allrank.iloc[:, 1], float(pbot))
     else:
         threshold = float(tbot)
-    for index, row in allrank.iterrows():
+    for index, row in allrank.iterrows():  # another loop if >1 metric
         if row[1] <= threshold:
             least_annt_list.append(row[0])
     print("IC threshold for least annotated genes:", threshold)
@@ -118,36 +133,53 @@ def filter_least_annotated(ranktable, tbot, pbot):
 
 
 def associated_ignorome(all_ppi, tppi, pppi, most, least, idtable, ranktable):
+    """
+    This function finds ignorome genes based on association with high information genes
+    :param all_ppi: path to STRING interaction file
+    :param tppi: absolute threshold
+    :param pppi: percentile threshold
+    :param most: list of top genes
+    :param least: list of bot genes
+    :param idtable: path to STRING alias file (or another id mapping table)
+    :param ranktable: path to ranktable
+    :return: interaction threshold, ignorome genes, highly-annotated genes interacting with ignorome
+    """
     all_ppi = pd.read_csv(all_ppi, header=0, delim_whitespace=True)
-    coexp = all_ppi.loc[all_ppi.coexpression > 0, ['protein1', 'protein2', 'coexpression']]
+    coexp = all_ppi.loc[all_ppi.coexpression > 0, ['protein1', 'protein2', 'coexpression']]  # or other interaction
     if pppi is not None:
         threshold = np.percentile(coexp['coexpression'], float(pppi))
     else:
         threshold = float(tppi)
     print("Coexpression threshold:", threshold)
 
-    # Filter most coexpressed
+    # Filter most coexpressed pairs
     coexp = all_ppi.loc[all_ppi.coexpression > threshold, ['protein1', 'protein2', 'coexpression']]
-    string_most, idmapping = common_to_string(idtable, ranktable, most)  # idmatch is path
+    string_most, idmapping = common_to_string(idtable, ranktable, most)
     # print("Top IC genes: ", string_most)
     string_least, idmapping = common_to_string(idtable, ranktable, least)
     # print("Bot IC genes: ", string_least)
+
+    # Get ignorome genes: have low knowledge/interest but interact strongly with high knowledge/interest genes
     most_least = coexp[coexp['protein1'].isin(string_most) & coexp['protein2'].isin(string_least)].\
         sort_values(by='coexpression', ascending=False)
     known_set = set(most_least.iloc[:, 0].tolist())
     ignorome_set = set(most_least.iloc[:, 1].tolist())
-    # print("\nStrongest most-least annotated pairs (biggest gap in knowledge):")
-    # print(most_least.head(10))
+    # print("\nStrongest most-least annotated pairs:\n", most_least.head(10))
 
-    # Print results with STRING and preferred name
+    # Print results with STRING ids, information value
     ignorome_table = idmapping[idmapping['string_id'].isin(ignorome_set)]
     known_table = idmapping[idmapping['string_id'].isin(known_set)]
-    print("\nTotal ignorome found:", len(ignorome_set), ignorome_table['alias'].to_list())
+    print("\nTotal ignorome found:", len(ignorome_set), ignorome_table['string_id'].to_list())
     print(ignorome_table)
     return threshold, ignorome_set, known_set
 
 
 def bipartite_graph(most, least):
+    """
+    :param most: list of top genes
+    :param least: list of bot genes
+    :return:
+    """
     G = nx.Graph()
     G.add_nodes_from(most, bipartite=0)
     G.add_nodes_from(least, bipartite=1)
@@ -155,10 +187,20 @@ def bipartite_graph(most, least):
 
 
 def common_to_string(idtable, ranktable, genelist):
-    ranktable = pd.read_csv(ranktable, sep="\t", header=0).rename(columns={"DB_Object_Symbol":"alias"})
-    idtable = pd.read_csv(idtable, sep="\t", header=0, names=["string_id", "alias", "source"], usecols=["string_id", "alias"])
-    idmapping = pd.merge(ranktable, idtable, on="alias", how="left")
-    idmapping = idmapping.drop_duplicates(keep="first").dropna()
+    """
+    This function takes UniProt protein symbols and returns STRING ids
+    :param idtable: path to STRING alias file
+    :param ranktable: path to ranktable (common gene names)
+    :param genelist: input common gene names
+    :return: gene STRING ids and idmapping table
+    """
+    ranktable = pd.read_csv(ranktable, sep="\t", header=0).rename(columns={"DB_Object_Symbol": "alias"})
+    idtable = pd.read_csv(idtable, sep="\t", header=0, names=["string_id", "alias", "source"],
+                          usecols=["string_id", "alias"])
+    all_idmapping = pd.merge(ranktable, idtable, on="alias", how="left")
+    idmapping = all_idmapping.drop_duplicates(keep="first").dropna()
+    # na_idmapping = all_idmapping[all_idmapping.isna().any(axis=1)]
+    # TODO: resolve NaN (input another mapping table from UniProt)
 
     returnlist = []
     for gene in genelist:
@@ -167,8 +209,13 @@ def common_to_string(idtable, ranktable, genelist):
 
 
 def get_network_api(calculated_threshold, gene_all_common, species):
-    """STRING API: retrieve interaction network from string-db.org"""
-    # TODO: replace hard code
+    """
+    This function uses STRING API to get interaction partners of target genes
+    :param calculated_threshold: interaction threshold
+    :param gene_all_common: list of ignorome genes
+    :param species: species ID
+    :return
+    """
     my_genes = gene_all_common
     params = {
         "identifiers": "%0d".join(my_genes),  # list of proteins in common names
@@ -187,6 +234,7 @@ def get_network_api(calculated_threshold, gene_all_common, species):
         if coexpression_score > float(calculated_threshold)/1000:
             # print
             print("\t".join([p1, p2, "coexpression (score. %.3f)" % coexpression_score]))
+    return
 
 
 def main():
@@ -203,7 +251,8 @@ def main():
     most = filter_most_annotated(options.ranktable, options.threshold_top, options.percentile_top)
     least = filter_least_annotated(options.ranktable, options.threshold_bot, options.percentile_bot)
     coexp_threshold, ignorome_list, known_list = associated_ignorome(options.stringppi, options.threshold_ppi,
-                                                                     options.percentile_ppi, most, least, options.idtable, options.ranktable)
+                                                                     options.percentile_ppi, most, least,
+                                                                     options.idtable, options.ranktable)
     species = options.idtable.split("/")[-1].split(".")[0]
     get_network_api(coexp_threshold, ignorome_list, species)
 
