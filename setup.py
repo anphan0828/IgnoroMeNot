04534b200bbb8780fd59c706cc182490026f1d9b
anphan0828 <ahphan@iastate.edu>
2022-08-15 15:47:53 -0500

    Added conda installation

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.2'
+VERSION = '0.0.4'
+DESCRIPTION = 'Ignorome finder'
+
+with open("README.md","r") as fh:
+    long_description=fh.read()
 
 # Setting up
 setup(
@@ -10,11 +14,14 @@ setup(
         version=VERSION,
         author="An Phan",
         author_email="<ahphan@iastate.edu>",
-        description="Finding ignorome",
-        long_description="This program outputs a list of ignorome genes highly associated with other well-annotated genes",
-        readme="README.md",
+        description=DESCRIPTION,
+        long_description=long_description,
+        long_description_content_type = 'text/markdown',
         packages=find_packages(),
         install_requires=[],
+        py_modules=["ignoromenot"],
+        package_dir={'': 'src'},
+        url="https://github.com/anphan0828/IgnoroMeNot.git",
         classifiers = [
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
